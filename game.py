bc114f6ec76e83a1bbcd7a9c91392eaba559c83d
Gabriel Kabbe <gabriel.kabbe@mail.de>
2019-06-15 21:02:27 +0200

    Python 2 -> 3

--- game.py
+++ game.py
@@ -1,129 +1,142 @@
 import numpy as np
 
+
 class Spielfeld:
-	def __init__(self, arr=None):
-		self.score = 0
-		if arr == None or arr != numpy.ndarray:
-			self.field = np.zeros((4,4), int)
-		else:
-			if len(arr.shape) != 2:
-				self.field = np.zeros((4,4), int)
-			else:
-				self.field = arr
+    def __init__(self, arr=None):
+        self.score = 0
+        if arr == None or arr != numpy.ndarray:
+            self.field = np.zeros((4, 4), int)
+        else:
+            if len(arr.shape) != 2:
+                self.field = np.zeros((4, 4), int)
+            else:
+                self.field = arr
 
-		self.dims = self.field.shape
+        self.dims = self.field.shape
 
-		self.add_numbers()
-		self.add_numbers()
+        self.add_numbers()
+        self.add_numbers()
 
+    def __repr__(self):
+        r = "--------------\n"
 
-	def __repr__(self):
-		r = "--------------\n"
+        for i in range(4):
+            r += "| "
+            for j in range(4):
+                if self.field[i, j] == 0:
+                    pass
+                else:
+                    r += str(self.field[i, j])
+                r += " | "
+            r += "\n"
+        r += "--------------\n"
 
-		for i in xrange(4):
-			r += "| "
-			for j in xrange(4):
-				if self.field[i,j] == 0:
-					pass
-				else:
-					r+=str(self.field[i,j])
-				r+=" | "
-			r += "\n"
-		r += "--------------\n"
+        return r
 
-		return r
+    def add_numbers(self):
+        freefields = []
+        for i, field in enumerate(self.field.flat):
+            if field == 0:
+                freefields.append(i)
 
-	def add_numbers(self):
-		freefields = []
-		for i, field in enumerate(self.field.flat):
-			if field == 0:
-				freefields.append(i)
+        ind = freefields[np.random.randint(len(freefields))]
 
-		ind = freefields[np.random.randint(len(freefields))]
+        if np.random.randint(2) == 0:
+            self.field[ind // self.dims[1], ind % self.dims[1]] = 2
+        else:
+            self.field[ind // self.dims[1], ind % self.dims[1]] = 4
 
-		if np.random.randint(2) == 0:
-			self.field[ind/self.dims[1], ind % self.dims[1]] = 2
-		else:
-			self.field[ind/self.dims[1], ind % self.dims[1]] = 4
+    def line_movable(self, arr, merged):
+        for i, elem in enumerate(arr):
+            if (
+                elem != 0
+                and i != 0
+                and (
+                    arr[i - 1] == 0
+                    or (arr[i - 1] == elem and i - 1 not in merged and i not in merged)
+                )
+            ):
+                return True
+        return False
 
+    def lines_movable(self, direction, merged):
+        if direction == "up":
+            for i in range(self.field.shape[1]):
+                if self.line_movable(self.field[:, i], merged[i]):
+                    return True
+            return False
+        elif direction == "down":
+            for i in range(self.field.shape[1]):
+                if self.line_movable(self.field[::-1, i], merged[i]):
+                    return True
+            return False
+        elif direction == "left":
+            for i in range(self.field.shape[0]):
+                if self.line_movable(self.field[i, :], merged[i]):
+                    return True
+            return False
+        else:
+            for i in range(self.field.shape[0]):
+                if self.line_movable(self.field[i, ::-1], merged[i]):
+                    return True
+            return False
 
-	def line_movable(self, arr, merged):
-		for i, elem in enumerate(arr):
-			if elem != 0 and i != 0 and (arr[i-1] == 0 or (arr[i-1] == elem and i-1 not in merged and i not in merged)):
-					return True
-		return False
+    def gameover(self, merged):
+        for direction in ["up", "down", "left", "right"]:
+            if self.lines_movable(direction, merged):
+                return False
+        return True
 
-	def lines_movable(self, direction, merged):
-		if direction == "up":
-			for i in xrange(self.field.shape[1]):
-				if self.line_movable(self.field[:,i], merged[i]):
-					return True
-			return False
-		elif direction == "down":
-			for i in xrange(self.field.shape[1]):
-				if self.line_movable(self.field[::-1,i], merged[i]):
-					return True
-			return False
-		elif direction == "left":
-			for i in xrange(self.field.shape[0]):
-				if self.line_movable(self.field[i,:], merged[i]):
-					return True
-			return False
-		else:
-			for i in xrange(self.field.shape[0]):
-				if self.line_movable(self.field[i,::-1], merged[i]):
-					return True
-			return False		
+    def win(self):
+        return 2048 in self.field
 
-	def gameover(self, merged):
-		for direction in ["up", "down", "left", "right"]:
-			if self.lines_movable(direction, merged):
-				return False
-		return True
+    def rowmove(self, arr, merged, score, factor):
+        for i in range(arr.size - 1):
+            if arr[i] == 0 and arr[i + 1] != 0:
+                arr[i] = arr[i + 1]
+                arr[i + 1] = 0
+            elif (
+                arr[i] != 0
+                and arr[i] == arr[i + 1]
+                and i not in merged
+                and i + 1 not in merged
+            ):
+                factor += 1
+                score += arr[i]
+                arr[i] += arr[i + 1]
+                arr[i + 1] = 0
+                merged.append(i)
+        return score, factor
 
-	def win(self):
-		return 2048 in self.field
+    def single_step(self, direction, merged, score, factor):
+        if direction == "up":
+            for i in range(self.field.shape[1]):
+                score, factor = self.rowmove(self.field[:, i], merged[i], score, factor)
+        elif direction == "down":
+            for i in range(self.field.shape[1]):
+                score, factor = self.rowmove(
+                    self.field[::-1, i], merged[i], score, factor
+                )
+        elif direction == "left":
+            for i in range(self.field.shape[0]):
+                score, factor = self.rowmove(self.field[i, :], merged[i], score, factor)
+        else:
+            for i in range(self.field.shape[0]):
+                score, factor = self.rowmove(
+                    self.field[i, ::-1], merged[i], score, factor
+                )
+        return score, factor
 
-	def rowmove(self, arr, merged, score, factor):
-		for i in xrange(arr.size-1):
-			if arr[i] == 0 and arr[i+1] != 0:
-				arr[i] = arr[i+1]
-				arr[i+1] = 0
-			elif arr[i] != 0 and arr[i] == arr[i+1] and i not in merged and i+1 not in merged:
-				factor += 1
-				score += arr[i]
-				arr[i] += arr[i+1]
-				arr[i+1] = 0
-				merged.append(i)
-		return score, factor
-
-	def single_step(self, direction, merged, score, factor):
-		if direction == "up":
-			for i in xrange(self.field.shape[1]):
-				score, factor = self.rowmove(self.field[:,i], merged[i], score, factor)
-		elif direction == "down":
-			for i in xrange(self.field.shape[1]):
-				score, factor = self.rowmove(self.field[::-1,i], merged[i], score, factor)
-		elif direction == "left":
-			for i in xrange(self.field.shape[0]):
-				score, factor = self.rowmove(self.field[i,:], merged[i], score, factor)
-		else:
-			for i in xrange(self.field.shape[0]):
-				score, factor = self.rowmove(self.field[i,::-1], merged[i], score, factor)
-		return score, factor
-
-	def full_move(self, direction):
-		merged = [[] for i in xrange(4)]
-		tempscore = 0
-		factor = 0
-		if self.lines_movable(direction, merged):
-			while self.lines_movable(direction, merged):
-				tempscore, factor = self.single_step(direction, merged)
-			self.add_numbers()
-		self.score += factor * tempscore
+    def full_move(self, direction):
+        merged = [[] for i in range(4)]
+        tempscore = 0
+        factor = 0
+        if self.lines_movable(direction, merged):
+            while self.lines_movable(direction, merged):
+                tempscore, factor = self.single_step(direction, merged)
+            self.add_numbers()
+        self.score += factor * tempscore
 
 
 def main(*args):
-	pass 
-
-
+    pass
--- main.py
+++ main.py
@@ -2,97 +2,131 @@ import curses
 import game
 import math
 
-curses_colors = [curses.COLOR_WHITE, curses.COLOR_CYAN, curses.COLOR_BLUE, curses.COLOR_GREEN, curses.COLOR_YELLOW, curses.COLOR_MAGENTA, curses.COLOR_RED, curses.COLOR_RED, curses.COLOR_RED, curses.COLOR_RED, curses.COLOR_RED]
+curses_colors = [
+    curses.COLOR_WHITE,
+    curses.COLOR_CYAN,
+    curses.COLOR_BLUE,
+    curses.COLOR_GREEN,
+    curses.COLOR_YELLOW,
+    curses.COLOR_MAGENTA,
+    curses.COLOR_RED,
+    curses.COLOR_RED,
+    curses.COLOR_RED,
+    curses.COLOR_RED,
+    curses.COLOR_RED,
+]
+
 
 class MainWindow:
+    def __init__(self, spielfeld):
+        self.spielfeld = spielfeld
+        self.width = 4
+        self.inp = "nix gedrueckt"
+        self.gameover = False
+        self.win = False
 
-	def __init__(self, spielfeld):
-		self.spielfeld = spielfeld
-		self.width = 4
-		self.inp = "nix gedrueckt"
-		self.gameover = False
-		self.win = False
+    def __enter__(self):
+        self.myscreen = curses.initscr()
+        curses.curs_set(0)
+        curses.start_color()
 
-	def __enter__(self):
-		self.myscreen = curses.initscr()
-		curses.curs_set(0)
-		curses.start_color()
+        for i in range(1, 11):
+            curses.init_pair(i, curses_colors[i], curses.COLOR_BLACK)
 
-		for i in xrange(1, 11):
-			curses.init_pair(i, curses_colors[i], curses.COLOR_BLACK) 
+    def __exit__(self, type, value, traceback):
+        curses.endwin()
 
-	def __exit__(self, type, value, traceback):
-		curses.endwin()			
+    def application(self):
+        while 1:
+            self.myscreen.clear()
+            self.check_input()
+            self.dims = self.myscreen.getmaxyx()
+            dims = self.myscreen.getmaxyx()
+            self.draw_surroundings()
+            if self.gameover:
+                self.draw("GAME OVER")
+            elif self.win:
+                self.draw("YOU WIN!")
+            else:
+                self.inp = self.myscreen.getch()
+            self.myscreen.refresh()
 
-	def application(self):
-		while 1:
-			self.myscreen.clear()
-			self.check_input()
-			self.dims = self.myscreen.getmaxyx()
-			dims = self.myscreen.getmaxyx()
-			self.draw_surroundings()
-			if self.gameover:
-				self.draw("GAME OVER")
-			elif self.win:
-				self.draw("YOU WIN!")
-			else:
-				self.inp = self.myscreen.getch()
-			self.myscreen.refresh()
+    def check_input(self):
+        if self.inp == 65:
+            s = "up"
+        elif self.inp == 66:
+            s = "down"
+        elif self.inp == 68:
+            s = "left"
+        elif self.inp == 67:
+            s = "right"
+        else:
+            s = "?"
 
-	def check_input(self):
-		if self.inp == 65:
-			s = "up"
-		elif self.inp == 66:
-			s = "down"
-		elif self.inp == 68:
-			s = "left"
-		elif self.inp == 67:
-			s = "right"
-		else:
-			s = "?"
+        if s != "?":
+            merged = [[] for i in range(4)]
+            tempscore, factor = 0, 0
+            if self.spielfeld.lines_movable(s, merged):
+                while self.spielfeld.lines_movable(s, merged):
+                    tempscore, factor = self.spielfeld.single_step(
+                        s, merged, tempscore, factor
+                    )
+                    self.myscreen.clear()
+                    self.draw_surroundings()
+                    self.myscreen.refresh()
+                    curses.napms(50)
+                self.spielfeld.add_numbers()
+                self.spielfeld.score += factor * tempscore
+            if self.spielfeld.gameover(merged):
+                self.gameover = True
+            elif self.spielfeld.win():
+                self.win = True
 
-		if s != "?":
-			merged = [[] for i in xrange(4)]
-			tempscore, factor = 0, 0
-			if self.spielfeld.lines_movable(s, merged):
-				while self.spielfeld.lines_movable(s, merged):
-					tempscore, factor = self.spielfeld.single_step(s, merged, tempscore, factor)
-					self.myscreen.clear()
-					self.draw_surroundings()
-					self.myscreen.refresh()
-					curses.napms(50)
-				self.spielfeld.add_numbers()
-				self.spielfeld.score += factor * tempscore
-			if self.spielfeld.gameover(merged):
-				self.gameover = True
-			elif self.spielfeld.win():
-				self.win = True
+    def draw(self, text):
+        self.myscreen.border(0)
+        self.myscreen.addstr(4, self.dims[1] // 2, text, curses.A_BOLD)
+        self.draw_field()
+        curses.napms(50)
 
-	def draw(self, text):
-		self.myscreen.border(0)
-		self.myscreen.addstr(4, self.dims[1]/2, text, curses.A_BOLD)
-		self.draw_field()			
-		curses.napms(50)
+    def draw_surroundings(self):
+        self.myscreen.border(0)
+        self.myscreen.addstr(1, 1, "2048", curses.color_pair(1))  # curses.A_BOLD)
+        self.draw_field()
 
+    def draw_field(self):
+        for i in range(5):
+            self.myscreen.hline(
+                self.dims[0] // 2 + (i - 2) * self.width,
+                self.dims[1] // 2 - 9,
+                "-",
+                8 * self.width - 1,
+            )
+        for i in range(5):
+            self.myscreen.vline(
+                self.dims[0] // 2 - 2 * self.width + 1,
+                self.dims[1] // 2 - 10 + i * 2 * self.width,
+                "|",
+                4 * self.width,
+            )
 
-	def draw_surroundings(self):
-		self.myscreen.border(0)
-		self.myscreen.addstr(1, 1, "2048", curses.color_pair(1))# curses.A_BOLD)
-		self.draw_field()			
+        for i, number in enumerate(self.spielfeld.field.flat):
+            if number != 0:
+                self.draw_number(number, i)
 
-	def draw_field(self):
-		for i in xrange(5):
-			self.myscreen.hline(self.dims[0]/2 +(i-2)*self.width, self.dims[1]/2-9, "-", 8*self.width-1)
-		for i in xrange(5):	
-			self.myscreen.vline(self.dims[0]/2 -2*self.width+1, self.dims[1]/2-10 +i*2*self.width, "|", 4*self.width)
+        self.myscreen.addstr(
+            self.dims[0] // 2 - 2 * self.width - 1,
+            self.dims[1] // 2 - 10,
+            "Score: " + str(self.spielfeld.score),
+        )
 
-		for i, number in enumerate(self.spielfeld.field.flat):
-			if number != 0:
-				self.draw_number(number, i)
-
-		self.myscreen.addstr(self.dims[0]/2 -2*self.width-1, self.dims[1]/2-10, "Score: "+str(self.spielfeld.score))
-
-
-	def draw_number(self, number, index):
-		pos = (self.dims[0]/2 + 2 + (index/4 - 2) * self.width, self.dims[1]/2-6 +(index %4) *2*self.width)
-		self.myscreen.addstr(pos[0], pos[1], str(number), curses.color_pair(int(math.log(number, 2)))| curses.A_BOLD)
+    def draw_number(self, number, index):
+        pos = (
+            self.dims[0] // 2 + 2 + (index // 4 - 2) * self.width,
+            self.dims[1] // 2 - 6 + (index % 4) * 2 * self.width,
+        )
+        self.myscreen.addstr(
+            pos[0],
+            pos[1],
+            str(number),
+            curses.color_pair(int(math.log(number, 2))) | curses.A_BOLD,
+        )
